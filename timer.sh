17244653bd2ad1d31c310fc19e52cca5f49a0ae6
byteboard <42851582+byteboard@users.noreply.github.com>
2019-02-25 16:47:27 -0800

    Add files via upload

---
+++ timer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+SECONDS=30
+END_DATE=$((`date +%s` + $SECONDS))
+while [ "$END_DATE" -ge `date +%s` ]; do
+   tmux set status-right "Time Remaining: $(date -u --date @$(($END_DATE - `date +%s`)) +%H:%M:%S)"
+   sleep 1
+done
